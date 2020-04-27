af32af1c9f9d4150ab539d8df64c738c191c9c7b
Elliot <elliotmrgn@gmail.com>
2020-04-27 19:44:27 -0400

    Update server.js
    changed home path

--- server.js
+++ server.js
@@ -26,7 +26,7 @@ const waitlist = [
 ]
 
 app.get("/", function(req, res) {
-    res.sendFile(path.join(__dirname, "index.html"));
+    res.sendFile(path.join(__dirname, "home.html"));
   });
 
 app.listen(PORT, function() {
